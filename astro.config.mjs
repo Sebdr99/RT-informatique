736774118c48648d5d96f25dd3e80a3e571a5fd0
Sebdr99 <161982473+Sebdr99@users.noreply.github.com>
2024-10-16 22:10:19 +0200

    Fix chemin CSS pour GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import icon from "astro-icon";
 
-
-
+// https://astro.build/config
 export default defineConfig({
-  base: '/RT-informatique/', // Chemin de base pour GitHub Pages
-  site: 'https://sebdr99.github.io/RT-informatique', // URL complète de ton site
-  integrations: [tailwind(), mdx(), sitemap()],
+  site: "https://astroship.web3templates.com",
+  integrations: [tailwind(), mdx(), sitemap(), icon()],
 });
